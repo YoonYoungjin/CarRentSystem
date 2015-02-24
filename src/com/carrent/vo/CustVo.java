24791b85a337c5ce07c21e7474bb5d6b2b311e9a
YoonYoungjin <younggin5619@gmail.com>
2015-02-24 16:46:12 +0900

    Init /20150224 /YYJ

---
+++ src/com/carrent/vo/CustVo.java
@@ -0,0 +1,124 @@
+package com.carrent.vo;
+
+public class CustVo {
+
+	String cust_id;
+	String cust_pwd;
+	String cust_name;
+	String cust_gender;
+	String cust_birthday;
+	String cust_phone;
+	String zip_code;
+	String bas_addr;
+	String ext_addr;
+	String cust_email;
+	String license_type;
+	String license_num;
+	String license_exp_date;
+	String license_exp_fdate;
+	String license_exp_tdate;
+	String reg_date;
+	String mod_date;
+	public String getCust_id() {
+		return cust_id;
+	}
+	public void setCust_id(String cust_id) {
+		this.cust_id = cust_id;
+	}
+	public String getCust_pwd() {
+		return cust_pwd;
+	}
+	public void setCust_pwd(String cust_pwd) {
+		this.cust_pwd = cust_pwd;
+	}
+	public String getCust_name() {
+		return cust_name;
+	}
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	public String getCust_gender() {
+		return cust_gender;
+	}
+	public void setCust_gender(String cust_gender) {
+		this.cust_gender = cust_gender;
+	}
+	public String getCust_birthday() {
+		return cust_birthday;
+	}
+	public void setCust_birthday(String cust_birthday) {
+		this.cust_birthday = cust_birthday;
+	}
+	public String getCust_phone() {
+		return cust_phone;
+	}
+	public void setCust_phone(String cust_phone) {
+		this.cust_phone = cust_phone;
+	}
+	public String getZip_code() {
+		return zip_code;
+	}
+	public void setZip_code(String zip_code) {
+		this.zip_code = zip_code;
+	}
+	public String getBas_addr() {
+		return bas_addr;
+	}
+	public void setBas_addr(String bas_addr) {
+		this.bas_addr = bas_addr;
+	}
+	public String getExt_addr() {
+		return ext_addr;
+	}
+	public void setExt_addr(String ext_addr) {
+		this.ext_addr = ext_addr;
+	}
+	public String getCust_email() {
+		return cust_email;
+	}
+	public void setCust_email(String cust_email) {
+		this.cust_email = cust_email;
+	}
+	public String getLicense_type() {
+		return license_type;
+	}
+	public void setLicense_type(String license_type) {
+		this.license_type = license_type;
+	}
+	public String getLicense_num() {
+		return license_num;
+	}
+	public void setLicense_num(String license_num) {
+		this.license_num = license_num;
+	}
+	public String getLicense_exp_date() {
+		return license_exp_date;
+	}
+	public void setLicense_exp_date(String license_exp_date) {
+		this.license_exp_date = license_exp_date;
+	}
+	public String getLicense_exp_fdate() {
+		return license_exp_fdate;
+	}
+	public void setLicense_exp_fdate(String license_exp_fdate) {
+		this.license_exp_fdate = license_exp_fdate;
+	}
+	public String getLicense_exp_tdate() {
+		return license_exp_tdate;
+	}
+	public void setLicense_exp_tdate(String license_exp_tdate) {
+		this.license_exp_tdate = license_exp_tdate;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getMod_date() {
+		return mod_date;
+	}
+	public void setMod_date(String mod_date) {
+		this.mod_date = mod_date;
+	}
+}
